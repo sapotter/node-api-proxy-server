38caab4a75906f215d5baaffa3eb6b2993a184be
Stephen A Potter <potter@hwi.buffalo.edu>
2023-10-02 21:30:16 -0400

    Added API_ROUTE to .env.

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const apicache = require('apicache')
 const API_BASE_URL = process.env.API_BASE_URL
 const API_KEY_NAME = process.env.API_KEY_NAME
 const API_KEY_VALUE = process.env.API_KEY_VALUE
-const API_ROUTE = 'data/2.5/weather'
+const API_ROUTE = process.env.API_ROUTE
 
 // Init cache
 let cache = apicache.middleware
@@ -25,7 +25,7 @@ router.get('/', cache('2 minutes'), async (req, res, next) => {
 
     // Log the request to the public API
     if (process.env.NODE_ENV !== 'production') {
-      console.log(`REQUEST: ${API_BASE_URL}?${params}`)
+      console.log(`REQUEST: ${API_BASE_URL}${API_ROUTE}?${params}`)
     }
 
     res.status(200).json(data)
